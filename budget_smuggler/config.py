5e47645de54fb518b6bdc6f788d7e318edea7082
Nathanael Gordon <nathanael@ionata.com.au>
2018-03-24 21:55:32 +1100

    Add AdminRenderer to default renderers

--- budget_smuggler/config.py
+++ budget_smuggler/config.py
@@ -2,4 +2,11 @@ from dj_core_drf.config import Config as BaseConfig
 
 
 class Config(BaseConfig):
-    pass
+    defaults = BaseConfig.defaults.copy()
+    defaults.update([
+        ('REST_FRAMEWORK__DEFAULT_RENDERER_CLASSES', (
+            'rest_framework.renderers.JSONRenderer',
+            'rest_framework.renderers.AdminRenderer',
+            'rest_framework.renderers.BrowsableAPIRenderer',
+        )),
+    ])
